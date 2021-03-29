b7424942d06430f58843f0f536df621a475dcf42
Valdemar Kostenko <vladimir.kostenko@playtech.com>
2021-03-29 19:07:36 +0300

    LH-46672: [MDL POC] draft digital-table

---
+++ src/digital-table/bet-place/bet-place.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PureComponent } from 'react';
+
+export class BetPlace extends PureComponent {
+  render() {
+    return (
+      <div className="mdl-mp-bet-place">
+
+      </div>
+    )
+  }
+}
---
+++ src/digital-table/digital-table.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PureComponent } from 'react';
+
+export class DigitalTable extends PureComponent {
+  render() {
+    return (
+      <div className="mdl-mp-digital-table">
+        
+      </div>
+    )
+  }
+}
